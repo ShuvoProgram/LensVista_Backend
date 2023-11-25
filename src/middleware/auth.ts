e634b4537dede4a03614c32d49d4ff91ae74e420
ShuvoProgram <shuvoprogramer@gmail.com>
2023-11-25 23:11:16 +0600

    🛡️ Fixed auth middleware; verified token and handled role-based access gracefully. 🚪 Improved auth flow, validating tokens and roles for a secure API gateway. 🔒 Strengthened auth validation, ensuring proper token handling and role checks. 🗝️ Enhanced auth middleware, refining token verification and role authorization. 🌐 Refined auth module; bolstered token verification and role-based access control.

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,44 +1,35 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-import { Secret } from 'jsonwebtoken';
-import config from '../config';
-import ApiError from '../error/ApiError';
-import { NextFunction, Response } from 'express';
-import { httpCode } from '../shared/httpCodes';
-import { jwtHelpers } from '../utils/jwtHelpers';
+/* eslint-disable no-console */
+import { NextFunction, Request, Response } from 'express'
+import { Secret } from 'jsonwebtoken'
+import ApiError from '../error/ApiError'
+import { httpCode } from '../shared/httpCodes'
+import { jwtHelpers } from '../utils/jwtHelpers'
+import config from '../config'
 
 const auth =
-    (...requiredRoles: string[]) =>
-    async (req: any, res: Response, next: NextFunction) => {
-        try {
-            //get authorization token
-            const token = req.headers.authorization;
-            if (!token) {
-                throw new ApiError(401, 'You are not authorized');
-            }
-            // verify token
-            let verifiedUser = null;
-            
-            verifiedUser = jwtHelpers.verifyToken(
-                token,
-                config.jwt.secret as Secret
-            );
-            req.user = verifiedUser;
-            // role diye guard korar jnno
+  (...requiredRoles: string[]) =>
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      //get authorization token
+      const token = req.header('Authorization')?.replace('Bearer ', '') // Extract the token from the request header
+      if (!token) {
+        throw new ApiError(httpCode.UNAUTHORIZED, 'You are not authorized')
+      }
 
-            console.log(verifiedUser);
-            if (
-                requiredRoles.length &&
-                !requiredRoles.includes(verifiedUser.role)
-            ) {
-                throw new ApiError(
-                    httpCode.FORBIDDEN,
-                    `You Don't  have permission to access this.`
-                );
-            }
-            next();
-        } catch (error) {
-            next(error);
-        }
-    };
+      //verify token
+      let verifiedUser = null
+
+    
+      verifiedUser = jwtHelpers.verifyToken(token, config.jwt.secret as Secret)
+      req.user = verifiedUser
+
+      if (requiredRoles.length && !requiredRoles.includes(verifiedUser.role)) {
+        throw new ApiError(httpCode.FORBIDDEN, 'Invalid Token')
+      }
+      next()
+    } catch (error) {
+      next(error)
+    }
+  }
 
 export default auth;
