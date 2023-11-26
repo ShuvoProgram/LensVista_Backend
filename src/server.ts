f38b51a3737a59a1337adb05818ecd33d11c6354
ShuvoProgram <shuvoprogramer@gmail.com>
2023-11-26 23:27:37 +0600

    🚀 Fired up server, handling uncaught exceptions & rejections for robustness. 🛠️ Enhanced error handling, ensuring server stability & graceful shutdown. 🔥 Improved server startup, managing exceptions & rejections seamlessly. 🌐 Optimized server launch, managing errors & shutdown gracefully. 🌟 Refined server initialization, handling exceptions & terminations smoothly.

---
+++ src/server.ts
@@ -0,0 +1,43 @@
+/* eslint-disable no-console */
+import config from './config';
+import app from './app';
+import { Server } from 'http';
+
+process.on('uncaughtException', error => {
+    console.log(error);
+    process.exit(1);
+});
+
+let server: Server;
+
+async function fire() {
+    try {
+        server = app.listen(config.port, () => {
+            console.log(
+                `Server Fire on http://localhost:${config.port}`
+            );
+        });
+    } catch (error) {
+        console.log('Something wrong happened staring the server ');
+    }
+
+    process.on('unhandledRejection', error => {
+        if (server) {
+            server.close(() => {
+                console.log(error);
+                process.exit(1);
+            });
+        } else {
+            process.exit(1);
+        }
+    });
+}
+
+fire();
+
+process.on('SIGTERM', () => {
+    console.log('SIGTERM received');
+    if (server) {
+        server.close();
+    }
+});
