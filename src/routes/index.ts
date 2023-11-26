ec75075a76add00f1e5ba80dbd08a828b738ab58
ShuvoProgram <shuvoprogramer@gmail.com>
2023-11-26 23:34:50 +0600

    🚀 Added API routes: authentication, user, service, booking, review, feedback, FAQ. 🛠️ Integrated various API routes for authentication, user management, services, and more. 🌐 Connected API routes: user, services, bookings, reviews, feedback, FAQ for smoother flow. 📡 Implemented API routes handling: user, services, bookings, reviews, feedback, FAQ. 🧩 Configured API routes for authentication, users, services, bookings, reviews, feedback, FAQ.

---
+++ src/routes/index.ts
@@ -0,0 +1,22 @@
+import express from 'express';
+import { TestRoute } from '../api/test/routes';
+import { AuthRoute } from '../api/auth/routes';
+import { UserRoute } from '../api/user/routes';
+import { ServiceRoute } from '../api/service/routes';
+import { BookingRoute } from '../api/booking/routes';
+import { ReviewRoute } from '../api/review/routes';
+import { FeedBackRoute } from '../api/feedback/routes';
+import { FaqRoute } from '../api/faq/routes';
+
+const router = express.Router();
+
+router.use('/', TestRoute);
+router.use('/auth', AuthRoute);
+router.use('/user', UserRoute);
+router.use('/service', ServiceRoute);
+router.use('/booking', BookingRoute);
+router.use('/review', ReviewRoute);
+router.use('/feedback', FeedBackRoute);
+router.use('/faq', FaqRoute);
+
+export default router;
