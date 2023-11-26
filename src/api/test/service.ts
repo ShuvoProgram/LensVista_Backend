904399ec0fe61bf9530147c75f5003b6f91ed204
ShuvoProgram <shuvoprogramer@gmail.com>
2023-11-26 23:40:20 +0600

    ✨ Updated TestService with author details and a welcoming message. 🌿🧛 🔄 Enhanced testFunction in TestService, including author and role info. 🚀 Added author, role, and a warm welcome message in TestService. 🌟 🌟 Improved TestService's testFunction with author and role details. 🌿🧛 Tweaked TestService's testFunction to include author info and a greeting.

---
+++ src/api/test/service.ts
@@ -0,0 +1,13 @@
+const testFunction = () => {
+    const data = {
+        author: 'Safar Khan Shuvo',
+        Role: 'Full-Stack Developer',
+        experience: '1 Year',
+        message: '🌿🧛 Welcome to Photography Service 🌿🧛'
+    };
+
+    return data;
+};
+export const TestService = {
+    testFunction
+};
